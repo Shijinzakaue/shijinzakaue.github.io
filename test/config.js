bcb7844cd0be516d31b5e624a1882533adb50601
Shijinzakaue <97389945+Shijinzakaue@users.noreply.github.com>
2026-03-02 21:12:14 +0800

    Add files via upload

---
+++ test/config.js
@@ -0,0 +1,98 @@
+const officialConfig = [
+  { name: "乃木坂46 公式サイト", link: "https://www.nogizaka46.com", group: "nogi" },
+  { name: "乃木坂46 ニュース", link: "https://www.nogizaka46.com/s/n46/news/list", group: "nogi" },  
+  { name: "乃木坂46 スケジュール", link: "https://www.sakurazaka46.com/s/s46/media/list", group: "nogi" },
+  { name: "乃木坂46 メンバー", link: "https://www.nogizaka46.com/s/n46/search/artist", group: "nogi" },
+  { name: "乃木坂46 ブログ", link: "https://www.nogizaka46.com/s/n46/diary/MEMBER", group: "nogi" },
+  { name: "乃木坂46 OFFICIAL WEB SHOP", link: "https://www.nogizaka46shop.com/", group: "nogi" },
+  { name: "乃木坂46 Mobile", link: "https://sp.nogizaka46.com", group: "nogi" }, 
+  { name: "のぎ動画", link: "https://www.sakurazaka46.com", group: "nogi" },
+  { name: "MYNOGI", link: "https://mynogi.snft.jp/", group: "nogi" },
+  { name: "EVENT 握手会/ミーグリ/CD購入イベント", link: "https://www.nogizaka46.com/s/n46/live?ima=0000&ct=HANDSHAKE", group: "nogi" },
+  { name: "乃木坂46 メッセージ", link: "https://message.nogizaka46.com/", group: "nogi" },
+  { name: "乃木坂工事中", link: "https://tv-aichi.co.jp/nogi-kou/", group: "nogi" },
+	
+  { name: "櫻坂46 公式サイト", link: "https://sakurazaka46.com/s/s46/", group: "saku" },  
+  { name: "櫻坂46 ニュース", link: "https://sakurazaka46.com/s/s46/news/list", group: "saku" },
+  { name: "櫻坂46 スケジュール", link: "https://sakurazaka46.com/s/s46/media/list", group: "saku" },
+  { name: "櫻坂46 メンバー", link: "https://sakurazaka46.com/s/s46/search/artist", group: "saku" },
+  { name: "櫻坂46 ブログ", link: "https://sakurazaka46.com/s/s46/diary/blog/list", group: "saku" },
+  { name: "櫻坂46 OFFICIAL GOODS STORE", link: "https://store.plusmember.jp/sakurazaka46/", group: "saku" },
+  { name: "MY PAGE", link: "https://sakurazaka46.com/s/s46/mypage", group: "saku" },
+  { name: "マネージャー日記", link: "https://sakurazaka46.com/s/s46/diary/managers_diary/list", group: "saku" },
+  { name: "さくみみ", link: "https://sakurazaka46.com/s/s46/diary/radio", group: "saku" },
+  { name: "EVENT ミーグリ", link: "https://sakurazaka46.com/s/s46/diary/event_page/list?ima=0000", group: "saku" },
+  { name: "櫻坂46 メッセージ", link: "https://message.sakurazaka46.com/", group: "saku" },
+  { name: "そこ曲がったら、櫻坂？", link: "https://www.tv-tokyo.co.jp/keyaki/", group: "saku" },
+
+  { name: "日向坂46 公式サイト", link: "https://www.hinatazaka46.com/s/official/", group: "hina" },
+  { name: "日向坂46 ニュース", link: "https://www.hinatazaka46.com/s/official/news/list", group: "hina" },
+  { name: "日向坂46 スケジュール", link: "https://www.hinatazaka46.com/s/official/media/list", group: "hina" },
+  { name: "日向坂46 メンバー", link: "https://www.hinatazaka46.com/s/official/search/artist", group: "hina" },
+  { name: "日向坂46 ブログ", link: "https://www.hinatazaka46.com/s/official/diary/member", group: "hina" },
+  { name: "日向坂46 OFFICIAL GOODS STORE", link: "https://store.plusmember.jp/hinatazaka46/", group: "hina" },
+  { name: "マイページ", link: "https://www.hinatazaka46.com/s/official/mypage", group: "hina" },
+  { name: "ひなたぼっこ日記", link: "https://www.hinatazaka46.com/s/official/diary/manager/list", group: "hina" },
+  { name: "日向坂46 ファンクラブ", link: "https://www.hinatazaka46.com/s/official/page/about_fanclub", group: "hina" },
+  { name: "EVENT ミーグリ", link: "https://www.hinatazaka46.com/s/official/diary/event/list?ima=0000", group: "hina" },
+  { name: "日向坂46 メッセージ", link: "https://message.hinatazaka46.com/", group: "hina" },
+  { name: "日向坂で会いましょう", link: "日向坂で会いましょう", group: "hina" },
+
+];
+
+const toolConfig = [
+  {
+    name: "",
+    desc: "",
+    detail: "",
+    tags: ["utility"],
+    groups: ["nogi", "saku", "hina"],
+    link: ""
+  },
+  {
+    name: "",
+    desc: "",
+    detail: "",
+    tags: ["media", "download"],
+    groups: ["nogi"],
+    link: ""
+  },
+  {
+    name: "",
+    desc: "",
+    detail: "",
+    tags: ["download", "media"],
+    groups: ["hina"],
+    link: ""
+  },
+  {
+    name: "",
+    desc: "",
+    detail: "",
+    tags: ["download", "utility"],
+    groups: ["saku"],
+    link: ""
+  },
+  {
+    name: "",
+    desc: "",
+    detail: "",
+    tags: ["utility"],
+    groups: ["nogi", "saku", "hina"],
+    link: ""
+  },
+  {
+    name: "",
+    desc: "",
+    detail: "",
+    tags: ["media"],
+    groups: ["nogi"],
+    link: ""
+  }
+];
+
+const tagLabels = {
+  "download": "下載",
+  "media": "影音",
+  "utility": "工具"
+};
